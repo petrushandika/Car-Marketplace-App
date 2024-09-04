793d389610e97a5172cdfc2f1fdecd96f653acec
petrushandika <petrushandikasinaga@gmail.com>
2024-09-04 23:56:55 +0700

    chore: setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Routes>
       <Route
         path="/"
-        element={<h1>Hello World</h1>}
+        element={<Home />}
+      />
+      <Route
+        path="/contact"
+        element={<Contact />}
       />
     </Routes>
   );
