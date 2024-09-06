256f105fe8b8f4fc1ec7f1e33c264506fc5d771f
petrushandika <petrushandikasinaga@gmail.com>
2024-09-06 07:14:51 +0700

    chore: category list component

--- src/components/shared/Data.jsx
+++ src/components/shared/Data.jsx
@@ -32,7 +32,66 @@ const Pricing = [
   },
 ];
 
+import suvIcon from "../../assets/icon/suv.png";
+import sedanIcon from "../../assets/icon/sedan.png";
+import hatchbackIcon from "../../assets/icon/hatchback.png";
+import electricIcon from "../../assets/icon/electric.png";
+import truckIcon from "../../assets/icon/truck.png";
+import convertibleIcon from "../../assets/icon/convertible.png";
+import hybridIcon from "../../assets/icon/hybrid.png";
+import coupeIcon from "../../assets/icon/coupe.png";
+import vanIcon from "../../assets/icon/van.png";
+
+const Category = [
+  {
+    id: 1,
+    name: "SUV",
+    icon: suvIcon,
+  },
+  {
+    id: 2,
+    name: "Sedan",
+    icon: sedanIcon,
+  },
+  {
+    id: 3,
+    name: "Hatchback",
+    icon: hatchbackIcon,
+  },
+  {
+    id: 4,
+    name: "Electric",
+    icon: electricIcon,
+  },
+  {
+    id: 5,
+    name: "Truck",
+    icon: truckIcon,
+  },
+  {
+    id: 6,
+    name: "Convertible",
+    icon: convertibleIcon,
+  },
+  {
+    id: 7,
+    name: "Hybrid",
+    icon: hybridIcon,
+  },
+  {
+    id: 8,
+    name: "Coupe",
+    icon: coupeIcon,
+  },
+  {
+    id: 9,
+    name: "Van",
+    icon: vanIcon,
+  },
+];
+
 export default {
   CarMakes,
   Pricing,
+  Category,
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Category from "../components/Category";
 
 function HomePage() {
   return (
     <div>
       {/* Header */}
       <Header />
-
       {/* Hero */}
       <Hero />
+      {/* Category */}
+      <Category />
     </div>
   );
 }
